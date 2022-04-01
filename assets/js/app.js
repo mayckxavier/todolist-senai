3f46a374bed48980c5fb7b871855bf14f1ae699a
Mayck Xavier <mayck.xavier@faci.ly>
2022-03-31 22:00:23 -0300

    Riscando a tarefa ao clicar no checkbox

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,38 +2,41 @@ function getItem(item) {
     return document.querySelector(item);
 }
 
-function addTodo(todoText){
-    const div = document.createElement("div");
-    div.classList.add("form-check");
+function addTodo(todoText) {
+    if (todoText != "") {
+        const div = document.createElement("div");
+        div.classList.add("form-check");
 
-    const input = document.createElement("input");
-    input.type = 'checkbox';
-    input.classList.add("form-check-input");
+        const input = document.createElement("input");
+        input.type = 'checkbox';
+        input.classList.add("form-check-input");
+        input.onclick = function () {
+            return checkTodo(this);
+        }
 
-    const label = document.createElement("label");
-    label.classList.add("form-check-label");
-    
-    const textItem = document.createTextNode(todoText); //Texto criado
-    
-    label.appendChild(textItem);
-    div.appendChild(input);
-    div.appendChild(label);
+        const label = document.createElement("label");
+        label.classList.add("form-check-label");
 
-    const listItem = document.createElement("li"); // <li></li>
-    listItem.appendChild(div); //<li>mais uma tarefa</li>
-    listItem.classList.add("list-group-item");
+        const textItem = document.createTextNode(todoText); //Texto criado
 
-    const todoList = getItem("#todo-list");
-    todoList.appendChild(listItem); //<ul>.....<li>mais uma tarefa</li></ul>
+        label.appendChild(textItem);
+        div.appendChild(input);
+        div.appendChild(label);
+
+        const listItem = document.createElement("li"); // <li></li>
+        listItem.appendChild(div); //<li>mais uma tarefa</li>
+        listItem.classList.add("list-group-item");
+
+        const todoList = getItem("#todo-list");
+        todoList.appendChild(listItem); //<ul>.....<li>mais uma tarefa</li></ul>
+    }
 }
 
 const inputTodo = getItem("#input-todo");
 
 inputTodo.addEventListener("keydown", (evento) => {
-    if(evento.code == 'Enter'){
-        console.log('Enter pressionado');
-        const inputValue = inputTodo.value;
-        addTodo(inputValue);
+    if (evento.code == 'Enter') {
+        addTodo(inputTodo.value);
         inputTodo.value = "";
     }
 });
@@ -41,14 +44,17 @@ inputTodo.addEventListener("keydown", (evento) => {
 const btnAddTodo = getItem("#btn-add-todo");
 btnAddTodo.addEventListener("click", (evento) => {
     const inputValue = inputTodo.value;
-
-    console.log(inputValue);
-    if (inputValue == "") {
-        console.log("String vazia");
-    } else {
-        addTodo(inputValue);
-        inputTodo.value = "";
-    }
+    addTodo(inputValue);
+    inputTodo.value = "";
 });
 
 
+function checkTodo(checkbox) {
+    const label = checkbox.nextElementSibling;
+
+    if (checkbox.checked == true) {
+        label.classList.add("todo-done");
+    } else {
+        label.classList.remove("todo-done");
+    }
+}
